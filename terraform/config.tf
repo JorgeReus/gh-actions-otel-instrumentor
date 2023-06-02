f53b496770aca790f71b3816306c7511e6c35ceb
Reus <j-g1996@live.com>
2023-06-01 21:17:26 -0600

    feat: Added conformance

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,6 +1,8 @@
 terraform {
   required_providers {
-    aws    = ">= 5.0.1"
-    random = ">= 3.5.1"
+    aws     = ">= 5.0.1"
+    random  = ">= 3.5.1"
+    archive = ">= 2.3.0"
   }
+  required_version = ">= 1.1.4"
 }
